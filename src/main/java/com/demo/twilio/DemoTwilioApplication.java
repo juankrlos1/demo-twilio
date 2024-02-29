ac3164826eef9e23f7b8d296b89635806dccf864
Juan Carlos <jkrlos.rock@gmail.com>
2024-02-28 23:11:58 -0600

    Initial commit

---
+++ src/main/java/com/demo/twilio/DemoTwilioApplication.java
@@ -0,0 +1,13 @@
+package com.demo.twilio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTwilioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoTwilioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/twilio/DemoTwilioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.twilio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoTwilioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
